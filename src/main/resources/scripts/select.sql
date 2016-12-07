f21e334117ef38a632fc8e4cb51295428bf6f609
Suresh Kumar <sureszkumar@gmail.com>
2016-12-07 20:11:53 +0100

    select sqls

---
+++ src/main/resources/scripts/select.sql
@@ -0,0 +1,12 @@
+select * from user_courses;
+select * from user_course_books;
+
+select * from course;
+select * from book;
+select * from course_books;
+
+
+select * from course_proficiency;
+select * from weekly_hours;
+select * from weekly_pages;
+select * from user;
